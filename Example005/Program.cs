7e0e9c4e9eee092ac9b8818fd1aa72558171e3e2
Berus Frager <gitmailru@yandex.ru>
2022-09-04 18:23:13 +0300

    Коммит добавлений в файлах папки проекта Example005

--- Example005/Program.cs
+++ Example005/Program.cs
@@ -10,12 +10,18 @@ int PowerRec(int a, int n)  // Рекурсивный метод
 {
     if (n == 0) return 1;
     else return a * PowerRec(a, n - 1);
-    return a;
 }
 int PowerRec1(int a, int n)  // Рекурсивный метод
 {
     return n == 0 ? 1 : PowerRec1(a, n - 1) * a;    // Альтернативная
 }                                                   // запись
+int PowerRecMath(int a, int n)  // Упрощение рекурсивного метода
+{
+    if (n == 0) return 1;
+    else if(n % 2 == 0) return PowerRecMath(a * a, n / 2);
+    else return PowerRecMath(a, n - 1) * a;
+}
 Console.WriteLine(PowerFor(2, 3));
 Console.WriteLine(PowerRec(2, 5));
-Console.WriteLine(PowerRec1(2, 10));
+Console.WriteLine(PowerRec1(2, 8));
+Console.WriteLine(PowerRecMath(2, 16));
