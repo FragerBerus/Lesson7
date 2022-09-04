c9f8b6a97e8417ef54f6b6e4677940c22001e5cb
Berus Frager <gitmailru@yandex.ru>
2022-09-04 17:11:29 +0300

    Коммит файлов папки проекта Example002

---
+++ Example002/Program.cs
@@ -0,0 +1,18 @@
+﻿// Собрать строку с числами от a до b, a >= b
+Console.Clear();
+string NumbersFor(int a, int b) // Без рекурсии
+{
+    string result = String.Empty;   // Возвращение пустой строки
+    for (int i = a; i >= b; i--)
+    {
+        result += $"{i} ";  // += прибавляет к левому операнду
+    }                       // правый и присваивает ему значение
+    return result;
+}
+string NumbersRec(int a, int b) // Использование рекурсии
+{
+    if (a >= b) return $"{a} " + NumbersRec(a - 1, b);
+    else return String.Empty;
+}
+Console.WriteLine(NumbersFor(10, 1));
+Console.WriteLine(NumbersRec(10, 1));
