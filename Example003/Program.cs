66dc77e2bef8cd2acbd933f81d4739a918b06c84
Berus Frager <gitmailru@yandex.ru>
2022-09-04 17:28:49 +0300

    Коммит файлов папки проекта Example003

---
+++ Example003/Program.cs
@@ -0,0 +1,15 @@
+﻿// Найти сумму чисел от 1 до n
+Console.Clear();
+int SumFor(int n)   // Итеративный метод
+{
+    int result = 0;
+    for (int i = 0; i <= n; i++) result += i;
+    return result;
+}
+int SumRec(int n)   // Рекурсивный метод
+{
+    if (n == 0) return 0;
+    else return n + SumRec(n - 1);
+}
+Console.WriteLine(SumFor(5));
+Console.WriteLine(SumRec(5));
