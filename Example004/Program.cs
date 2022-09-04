15fccb281ea4edb95fa20a0a117cdc2d2c16720c
Berus Frager <gitmailru@yandex.ru>
2022-09-04 17:47:28 +0300

    Коммит файлов папки проекта Example004

---
+++ Example004/Program.cs
@@ -0,0 +1,15 @@
+﻿// Найти факториал числа n
+Console.Clear();
+int FactorialFor(int n) // Итеративный метод
+{
+    int result = 1;
+    for (int i = 1; i <= n; i++) result *= i;
+    return result;
+}
+int FactorialRec(int n) // Рекурсивный метод
+{
+    if (n == 1) return 1;
+    else return n *= FactorialRec(n - 1);
+}
+Console.WriteLine(FactorialFor(5));
+Console.WriteLine(FactorialRec(5));
