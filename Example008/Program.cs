138e838b8b7950657cadee0f61440ff644587a6a
Berus Frager <gitmailru@yandex.ru>
2022-09-06 11:43:00 +0300

    Коммит файлов папки проекта Example008

---
+++ Example008/Program.cs
@@ -0,0 +1,10 @@
+﻿// Игра в пирамидки. Строковые переменные - шпили. Целая
+// переменная - количество блинов.
+Console.Clear();
+void Towers(string with = "1", string on = "3", string some = "2", int count = 4)
+{
+    if(count > 1) Towers(with, some, on, count -1);
+    Console.WriteLine($"{with} >> {on}");
+    if(count > 1) Towers(some, on, with, count - 1);
+}
+Towers();
